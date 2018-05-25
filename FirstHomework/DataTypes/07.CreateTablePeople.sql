85cb2eceed107d6beb614bb139ab69911db6f5bc
warglaivee <warglaivegm@gmail.com>
2018-05-25 22:53:54 +0300

    up

--- FirstHomework/DataTypes/07.CreateTablePeople.sql
+++ FirstHomework/DataTypes/07.CreateTablePeople.sql
@@ -1,8 +1,10 @@
 USE Minions
 CREATE TABLE People(
-Id INT IDENTITY,
+Id INT IDENTITY PRIMARY KEY,
 [Name] NVARCHAR(50) NOT NULL,
 Picture IMAGE,
 Height DECIMAL(15, 2),
-Gender
+Gender varchar(10) NOT NULL CHECK (Gender IN('m', 'f')),
+Birthday DATETIME NOT NULL,
+Biography NVARCHAR(MAX)
 )
