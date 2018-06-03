91446af25f90a087dbee14098f997f931c47192c
warglaivee <warglaivegm@gmail.com>
2018-06-03 17:40:05 +0300

    up

--- Built-in Functions/Build-In-Functions/10. Countries Holding 'A'.sql
+++ Built-in Functions/Build-In-Functions/10. Countries Holding 'A'.sql
@@ -2,5 +2,5 @@ USE Geography
 SELECT CountryName AS [Country Name], IsoCode AS [Iso Code] 
 FROM Countries
 --full string letters minus replaced string letters(after replacement with '')
-WHERE LEN(CountryName) - LEN(REPLACE(CountryName, 'a','')) >= 3
+WHERE CountryName LIKE '%a%a%a%'
 ORDER BY [Iso Code]
--- Built-in Functions/Build-In-Functions/11. Mix of Peak and River Names.sql
+++ Built-in Functions/Build-In-Functions/11. Mix of Peak and River Names.sql
@@ -1,1 +1,5 @@
-SELECT PeakName FROM Peaks
+--USE Geography
+SELECT PeakName, RiverName, LOWER(CONCAT(LEFT(PeakName, LEN(Peakname) - 1), RiverName)) AS [Mix]
+FROM Peaks, Rivers
+WHERE RIGHT(PeakName, 1) = LEFT(RiverName, 1)
+ORDER BY Mix
