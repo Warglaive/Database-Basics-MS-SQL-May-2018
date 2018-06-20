972ce5155b4143f85a64f626ec840a51157df443
warglaivee <warglaivegm@gmail.com>
2018-06-20 13:24:32 +0300

    up

--- Database Programmability and Transactions/Database Programmability and Transactions/13. Cash in User Games Odd Rows .sql
+++ Database Programmability and Transactions/Database Programmability and Transactions/13. Cash in User Games Odd Rows .sql
@@ -2,3 +2,15 @@
 CREATE FUNCTION ufn_CashInUsersGames(@gameName NVARCHAR(MAX))
 RETURNS TABLE
 AS
+RETURN(
+SELECT SUM(e.Cash) AS [SumCash] 
+	FROM (
+		SELECT ug.Cash, ROW_NUMBER() OVER(ORDER BY ug.Cash DESC) AS [row] 
+			FROM Games AS g
+			JOIN UsersGames AS ug
+			ON ug.GameId = g.Id
+			WHERE g.[Name] = @gameName) AS e
+			WHERE e.row % 2 = 1
+)
+--DROP FUNCTION ufn_CashInUsersGames
+--SELECT * FROM [dbo].ufn_CashInUsersGames('Love in a mist')
