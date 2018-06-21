d974d8b473d4ec834c92749f0870480989de7762
warglaivee <warglaivegm@gmail.com>
2018-06-21 20:09:40 +0300

    up

---
+++ Databases MSSQL Server Exam - 22 October 2017/Databases MSSQL Server Exam - 22 October 2017/02. Insert .sql
@@ -0,0 +1,14 @@
+USE ReportService
+INSERT INTO Employees
+VALUES
+('Marlo', 'O’Malley', 'M', '9/21/1958', 1),
+('Niki','Stanaghan', 'F', '11/26/1969', 4),
+('Ayrton','Senna', 'M', '03/21/1960', 9),
+('Ronnie','Peterson', 'M', '02/14/1944', 9),
+('Giovanna','Amati', 'F', '07/20/1959', 5)
+
+INSERT INTO Reports
+VALUES
+()
+USE MASTER
+DROP DATABASE ReportService
